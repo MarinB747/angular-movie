7022770158dab67d8030cbbd8eece320cd3d1877
Marin Blažević <blazevic.marin747@gmail.com>
2021-12-10 13:32:55 +0100

    movies query params

--- src/app/movies.service.ts
+++ src/app/movies.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MoviesService {
-  configUrl = `https://api.themoviedb.org/3/movie/now_playing?api_key=f032274d9c9fd3adb40ade7cdc65485b&language=en-US&page=1`;
+  page = 1;
   getMovies(): Observable<any> {
-    return this.http.get(this.configUrl);
+    console.log(this.page);
+    return this.http.get(
+      `https://api.themoviedb.org/3/movie/now_playing?api_key=f032274d9c9fd3adb40ade7cdc65485b&language=en-US&page=${this.page}`
+    );
   }
   constructor(private http: HttpClient) {}
 }
--- src/app/views/movies/movies.component.ts
+++ src/app/views/movies/movies.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MoviesService } from 'src/app/movies.service';
+import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
@@ -10,15 +11,42 @@ export class MoviesComponent implements OnInit {
   details = 'Details';
   prev = 'Previous';
   next = 'Next';
-  constructor(private moviesService: MoviesService) {}
+  constructor(
+    private moviesService: MoviesService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.getMovies();
-    console.log(this.movies);
   }
   getMovies(): any {
+    this.movies.length = 0;
     this.moviesService
       .getMovies()
       .subscribe((data: any) => this.movies.push(data.results));
+    console.log(this.movies);
+  }
+  onNext() {
+    if (this.moviesService.page === 50) {
+      this.moviesService.page = 1;
+    } else {
+      this.moviesService.page = this.moviesService.page + 1;
+    }
+    this.router.navigate(['/movies'], {
+      queryParams: { page: this.moviesService.page },
+    });
+    this.ngOnInit();
+  }
+  onPrev() {
+    if (this.moviesService.page === 1) {
+      this.moviesService.page = 50;
+    } else {
+      this.moviesService.page = this.moviesService.page - 1;
+    }
+    this.router.navigate(['/movies'], {
+      queryParams: { page: this.moviesService.page },
+    });
+    this.ngOnInit();
   }
 }
